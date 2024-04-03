c57320ed209d1fe305a99065ed7f4a5cab0ad226
Javi17mod <123207842+Javi17mod@users.noreply.github.com>
2024-04-03 20:00:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 import { aterbot }  from "aterbot";
 
-aterbot()
+const bot = aterbot();
+
+            // Obtén el elemento donde se mostrará el output
+            const outputElement = "<h1>On</h1>;
+
+            // Renderiza aterbot en el elemento de salida
+            bot.render(outputElement);
